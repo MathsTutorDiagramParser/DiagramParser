69c23876812f3b49b6d8cde4ab577627f06ed2fc
wiranji <wiranjidinelk.13@cse.mrt.ac.lk>
2017-08-06 15:00:53 +0530

    Modify rest api to get answer from editor

---
+++ src/main/java/com/tutor/service/RestService/SVGAnswerServiceImpl.java
@@ -0,0 +1,42 @@
+package com.tutor.service.RestService;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * Created by Wiranji Dinelka on 8/6/2017.
+ */
+public class SVGAnswerServiceImpl implements SVGAnswerService {
+    @Override
+    public void storeSVGInput(String inputStr) throws IOException, ParserConfigurationException, SAXException, TransformerException {
+
+        String decodedStr = java.net.URLDecoder.decode(inputStr, "UTF-8");
+        String xmlString = decodedStr.substring(decodedStr.indexOf("=")+1);
+
+        // To transform string into xml
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(new StringReader(xmlString)));
+
+        // Write the parsed document to an xml file
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+
+        StreamResult result =  new StreamResult(new File("E:\\FYP\\implementation\\parser1\\DiagramParser\\src\\main\\resources\\com\\answerFile\\svgresult.xml"));
+        transformer.transform(source, result);
+    }
+}
