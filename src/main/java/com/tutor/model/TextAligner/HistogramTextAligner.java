be1f0ba6b09a9cd22ba7b7fbb182ddc2734cf4f8
Vithusha <vithusha.13@cse.mrt.ac.lk>
2017-08-07 05:46:17 +0530

    Added Text Alignment for Numberline .

--- src/main/java/com/tutor/model/TextAligner/HistogramTextAligner.java
+++ src/main/java/com/tutor/model/TextAligner/HistogramTextAligner.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class HistogramTextAligner {
 
     public AbstractHistogramStructure alignTextToHistogram(AbstractHistogramStructure abstractHistogramStructure, List<Text> textList) {
-
         return null;
     }
 }
