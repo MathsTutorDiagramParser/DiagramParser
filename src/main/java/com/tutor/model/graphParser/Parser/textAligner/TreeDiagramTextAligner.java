09093f1bdf1c0bb9a04a4f22aa21fd62acc934e3
Vithusha <vithusha.13@cse.mrt.ac.lk>
2017-08-09 08:46:20 +0530

    Adding tree Diagram text alignment

--- src/main/java/com/tutor/model/graphParser/Parser/textAligner/TreeDiagramTextAligner.java
+++ src/main/java/com/tutor/model/graphParser/Parser/textAligner/TreeDiagramTextAligner.java
@@ -1,7 +1,10 @@
-package com.tutor.model.graphParser.Parser.textAligner;
+package com.tutor.model.TextAligner;
 
 import com.tutor.model.graphParser.DiagramStructure.AbstractDiagramStructure;
+
 import com.tutor.model.graphParser.DiagramStructure.TreeDiagram.AbstractTreeDiagramStructure;
+import com.tutor.model.graphParser.DiagramStructure.TreeDiagram.TreeBranch;
+import com.tutor.model.graphParser.DiagramStructure.TreeDiagram.TreeGraph;
 import com.tutor.model.graphicalPOJOObject.Text.Text;
 import com.tutor.model.graphicalPOJOObject.line.Line;
 
@@ -13,47 +16,83 @@ import java.util.List;
 public class TreeDiagramTextAligner extends TextAligner{
 
 
-    public AbstractTreeDiagramStructure abstractDiagramStructure;
-    public AbstractDiagramStructure alignTextToTreeDiagram(AbstractDiagramStructure abstractDiagramStructure, List<Text> texts){
-        this.abstractDiagramStructure = (AbstractTreeDiagramStructure)abstractDiagramStructure;
+    public AbstractTreeDiagramStructure abstractTreeDiagramStructure;
+    public List<Text> textList;
+    TextAligner aligner = new TextAligner();
+    List<TreeGraph> graphList;
 
-
-
+    public AbstractDiagramStructure alignTextToTreeDiagram(AbstractDiagramStructure abstractDiagramStructure, List<Text> textList){
+        this.abstractTreeDiagramStructure = (AbstractTreeDiagramStructure)abstractDiagramStructure;
+        this.textList = textList;
+        this.graphList = abstractTreeDiagramStructure.getTreeGraphArrayList();
+        for(TreeGraph graph: graphList) {
+            matchDescriptionText(graph.getNode().getLeftTreeBranch());
+            matchProbabilityText(graph.getNode().getLeftTreeBranch());
+            matchDescriptionText(graph.getNode().getRightTreeBranch());
+            matchProbabilityText(graph.getNode().getRightTreeBranch());
+        }
         return abstractDiagramStructure;
     }
 
 
-    public Text matchDescriptionText(Line line){
-
-
-
+    public void matchDescriptionText(TreeBranch branch){
+        Line line = branch.getAngleLine();
         Text text = null;
+        Double minDistance = Double.POSITIVE_INFINITY;
+        for(Text desText: textList){
+            boolean isInside = isInsideLengthTolerance(line, desText);
+            if(isInside){
+                if(minDistance > aligner.findLineToPointDistance(line, desText)){
+                    minDistance = aligner.findLineToPointDistance(line, desText);
+                    text = desText;
+                }
+            }
+        }
+
+        if (minDistance != Double.POSITIVE_INFINITY & text.isAttached() ==false ){
+           branch.setOutCome(text);
+            text.setAttached(true);
+        }
 
 
-        return text;
     }
 
-    public Text matchProbabilityText(Line line){
-
+    public void matchProbabilityText(TreeBranch branch){
+        Line line = branch.getAngleLine();
         Text text = null;
+        Double minDistance = Double.POSITIVE_INFINITY;
+        for (Text probText :textList){
+            if(!probText.isAttached()){
+                if(isProbabilityMatch(line,probText)){
+                    if(minDistance > findProbabilityPointDistance(line , probText)){
+                        minDistance = findProbabilityPointDistance(line , probText);
+                        text = probText;
+                    }
+                }
+            }
+        }
 
-        return text;
+        if (minDistance != Double.POSITIVE_INFINITY & text.isAttached() ==false ){
+            branch.setProbability(text);
+            text.setAttached(true);
+        }
     }
 
     public boolean minimalComplexitySolution(){
-
-
         return false;
     }
 
-    public boolean isInside(double textCordinate_X, double textCordinate_Y, double tickCordinate_X , double tickCordinate_Y, double x_radius, double y_radius){
-        double ellipseBoundary = (Math.pow((textCordinate_X - tickCordinate_X),2)/ Math.pow(x_radius,2)) +
-                (Math.pow((textCordinate_Y - tickCordinate_Y),2)/ Math.pow(y_radius,2));
+    public boolean isInsideEllipse(double textCordinate_X, double textCordinate_Y, double center_X , double center_Y,
+                                   double x_radius, double y_radius){
+        double ellipseBoundary = (Math.pow((textCordinate_X - center_X),2)/ Math.pow(x_radius,2)) +
+                (Math.pow((textCordinate_Y - center_Y),2)/ Math.pow(y_radius,2));
         if (ellipseBoundary <= 1){
             return true;
         }
         else {return false;}
     }
+
+
 
 
 }
